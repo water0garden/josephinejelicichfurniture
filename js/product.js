7f00d1bbc821bda5afb168f8bdc2d6c25f26441b
water0garden <57920829+water0garden@users.noreply.github.com>
2025-08-20 12:42:51 +1000

    new edit

--- js/product.js
+++ js/product.js
@@ -73,7 +73,7 @@ function displayProduct(product) {
   document.getElementById("product-detail").innerHTML = `
     ${imagesHtml}
     <p class="product-title">${product.title}</p>
-    <p class="shop-paragraph">${product.descriptionHtml}</p>
+    <div class="shop-paragraph">${product.descriptionHtml}</div>
     <p class="product-variant">Options</p>
     ${variantsHtml}
   `;
