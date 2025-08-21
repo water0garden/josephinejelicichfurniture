4e9ff885ad02d5e085895f3d36d4636f0cadd536
water0garden <57920829+water0garden@users.noreply.github.com>
2025-08-21 14:41:24 +1000

    git commit

--- js/shop.js
+++ js/shop.js
@@ -61,14 +61,14 @@ function renderProducts(products) {
     const currency = product.variants.edges[0]?.node.price.currencyCode || "";
     const handle = product.handle;
     return `
-      <article class="product-list">
+    //   
         <figure class="product">
           <a href="productdetail/index.html?handle=${handle}"><img src="${image}" alt="${alt}"></a>
         </figure>
         <figcaption>
           <a href="productdetail/index.html?handle=${handle}" class="shop-detail-link"><p class="product-title">${product.title}</p></a>
         </figcaption>
-      </article>
+      
     `;
   }).join("");
 }
