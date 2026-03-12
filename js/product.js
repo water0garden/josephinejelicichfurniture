b8b2c7337e7edf5ff6c924ae470f3a989b2d2863
water0garden <57920829+water0garden@users.noreply.github.com>
2026-03-12 16:05:16 +1300

    test

--- js/product.js
+++ js/product.js
@@ -85,7 +85,7 @@ function displayProduct(product) {
   // If multiple variants, show options with product.title and variant title, price, and add to cart
   let variantsHtml = product.variants.edges.map(variant =>
     `<div class="product-variant">
-      <span class="p text-frag" >${product.title} - ${variant.node.title}</span>
+      <span class="p text-frag">${variant.node.title}</span>
       <span class="product-price text-frag">${variant.node.price.amount} ${variant.node.price.currencyCode}</span>
       <button onclick="addToCart('${variant.node.id}', '${product.title} - ${variant.node.title}', '${variant.node.price.amount}', '${variant.node.price.currencyCode}')">Add to Cart</button>
     </div>`
