3078b770591bee80e5ac60d5b04007ebb569abf0
water0garden <57920829+water0garden@users.noreply.github.com>
2026-03-26 15:49:52 +1300

    test

--- js/drop.js
+++ js/drop.js
@@ -1,23 +1,36 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var dropdownTrigger = document.querySelector('.dropbtn');
+//   var dropdownTrigger = document.querySelector('.dropbtn');
 
-  dropdownTrigger.addEventListener('click', myFunction);
+//   dropdownTrigger.addEventListener('click', myFunction);
+
+//   function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// })
 
   function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
       }
     }
-  }
-  }
-
-
-})
+    })
