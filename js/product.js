e34fa5928495bda4397a59918c2038d16557d76b
water0garden <57920829+water0garden@users.noreply.github.com>
2026-06-13 10:36:39 -1000

    test

--- js/product.js
+++ js/product.js
@@ -97,8 +97,7 @@ let imagesHtml = `<ul class="image-scroll-list">` +
 let variantsHtml = product.variants.edges.map(variant =>
   `<div class="product-variant" style="padding-top: 0.5rem;">
     <span class="p">${variant.node.title}</span>
-  <span class="product-price">$${variant.node.price.amount}</span>
-    <button onclick="addToCart(
+<span class="product-price">$${parseFloat(variant.node.price.amount).toFixed(2)}</span>    <button onclick="addToCart(
     '${variant.node.id}',
     '${product.title} — ${variant.node.title}',
     '${variant.node.price.amount}',
